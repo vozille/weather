2fdcf14ee202bea54d9bd544738c9e2e69f8534c
Anwesh Mohanty <anwesh063@gmail.com>
2016-10-01 13:47:30 +0530

    version 0.1

--- weather.py
+++ weather.py
@@ -2,46 +2,71 @@ import requests
 import json
 import signal
 import gi
-import time
 from gi.repository import Gtk, AppIndicator3, GObject
 
 gi.require_version('Gtk', '3.0')
 
 
-# fix this location stuff
+# add error handling
 
-# send_url = 'http://freegeoip.net/json'
-# request = requests.get(send_url)
-# json_data = json.loads(request.text)
-# lat = json_data['latitude']
-# lon = json_data['longitude']
-# print(lat, lon)
-#
-# params = {'latlng': {str(lat) + ',' + str(lon)}, 'sensor': 'false', 'key': 'AIzaSyBNg2yWiDaEa3lZL0yXnM9ubC-ZOPCWa7I'}
-# city_details = requests.get('https://maps.googleapis.com/maps/api/geocode/json?', params=params)
-# print(city_details.text)
+def get_city():
+    send_url = 'http://freegeoip.net/json'
+    try:
+        request = requests.get(send_url)
+        if request.status_code == 200:
+            json_data = json.loads(request.text)
+            lat = json_data["latitude"]
+            long = json_data["longitude"]
+            return json_data["city"]
+        else:
+            return None
+    except requests.ConnectionError:
+        return None
 
 
-def get_weather(city):
+def get_weather():
+    city = get_city()
+    # no internet
+    if city is None:
+        print('no internet connection')
+        with open('./data.txt') as file:
+            data = json.load(file)
+        return data
 
-    params = {'APPID': '1d02acb0a01b4471272e58de491565e4', 'q': city}
-    city_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?', params=params)
-    weather_details = json.loads(city_weather.text)
-    print(weather_details)
-    print(weather_details["weather"][0]["icon"])
-    icon = requests.get('http://openweathermap.org/img/w/' + weather_details["weather"][0]["icon"] + '.png',
-                        stream=True)
-    if icon.status_code == 200:
-        with open('./icon.png', 'wb') as f:
-            for chunk in icon:
-                f.write(chunk)
-    data = {}
-    data["city"] = city
-    data["temp"] = str(
-        round(float((weather_details["main"]["temp_max"]) + float(weather_details["main"]["temp_min"])) / 2 - 273.15, 2)
-    )
-    data["weather"] = weather_details["weather"][0]["description"]
-    return data
+    try:
+        params = {'APPID': '1d02acb0a01b4471272e58de491565e4', 'q': city.lower(), 'units' : 'metric'}
+        city_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?', params=params)
+        # internet connection lost midway
+        if city_weather.status_code == 200:
+            weather_details = json.loads(city_weather.text)
+            print(weather_details)
+            icon = requests.get('http://openweathermap.org/img/w/' + weather_details["weather"][0]["icon"] + '.png',
+                                stream=True)
+            if icon.status_code == 200:
+                with open('./icon.png', 'wb') as f:
+                    for chunk in icon:
+                        f.write(chunk)
+            data = dict()
+            data["city"] = city
+            data["temp"] = str(
+                round(float((weather_details["main"]["temp_max"]) + float(weather_details["main"]["temp_min"])) / 2, 2)
+            )
+            data["weather"] = weather_details["weather"][0]["description"]
+            data["humidity"] = str(weather_details["main"]["humidity"])
+            with open('./data.txt', 'w') as file:
+                json.dump(data, file)
+
+            return data
+        else:
+            print('no internet connection')
+            with open('./data.txt') as file:
+                data = json.load(file)
+                return data
+    except requests.ConnectionError:
+        print('we lost internet connection')
+        with open('./data.txt') as file:
+            data = json.load(file)
+            return data
 
 
 class Indicator:
@@ -52,27 +77,26 @@ class Indicator:
             self.app, iconpath,
             AppIndicator3.IndicatorCategory.OTHER)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        self.indicator.set_menu(self.create_menu())
-        self.cnt = 0
-        self.indicator.set_label(data["city"] + " - " + data["temp"] + " °C, " + data["weather"], self.app)
-        GObject.timeout_add(1000*15, self.refresh)
+        self.indicator.set_menu(self.create_menu(data))
+        self.indicator.set_label(data["city"] + " - " + data["temp"] + " °C", self.app)
+        GObject.timeout_add(1000*60, self.refresh)
 
     def refresh(self):
-        data = get_weather(city_lists[self.cnt])
-        self.indicator.set_menu(self.create_menu())
-        self.indicator.set_label(data["city"] + " - " + data["temp"] + " °C, " + data["weather"], self.app)
+        data = get_weather()
+        self.indicator.set_menu(self.create_menu(data))
+        self.indicator.set_label(data["city"] + " - " + data["temp"] + " °C", self.app)
         return True
 
-    def create_menu(self):
+    def create_menu(self, data):
         menu = Gtk.Menu()
         # menu item 1
-        item_1 = Gtk.MenuItem('')
-        # item_about.connect('activate', self.about)
+        item_1 = Gtk.MenuItem('today: '+data["weather"])
         menu.append(item_1)
-        # separator
+        item_2 = Gtk.MenuItem('humidity: '+data["humidity"]+'%')
+        menu.append(item_2)
         menu_sep = Gtk.SeparatorMenuItem()
         menu.append(menu_sep)
-        # quit
+
         item_quit = Gtk.MenuItem('Quit')
         item_quit.connect('activate', self.quit)
         menu.append(item_quit)
@@ -83,12 +107,13 @@ class Indicator:
     def quit(self, src):
         exit(0)
 
-city_lists = ['cuttack', 'bhubaneswar', 'puri', 'bangalore', 'kolkata']
-i = 1
-stats = get_weather(city_lists[i])
-widget = Indicator(stats)
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-Gtk.main()
+
+def main():
+    stats = get_weather()
+    widget = Indicator(stats)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    Gtk.main()
 
 
-
+if __name__ == '__main__':
+    main()
